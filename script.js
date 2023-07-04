f07ec9daa7f3e86c5ccb7ab870d99af635401d95
KobeT1 <137968625+KobeT1@users.noreply.github.com>
2023-07-04 11:23:45 +0800

    fixed format

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-const navLinks = document.querySelectorAll('nav ul li a');
+const navLinks = document.querySelectorAll("nav ul li a");
 
-    navLinks.forEach(link => {
-      link.addEventListener('click', e => {
-        e.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
-      });
-    });
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
 
-    const contactForm = document.getElementById('contact-form');
+const contactForm = document.getElementById("contact-form");
 
-    contactForm.addEventListener('submit', e => {
-      e.preventDefault();
-    });
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
-    const galleryImages = document.querySelectorAll('.gallery img');
-    const lightbox = document.createElement('div');
-    lightbox.className = 'lightbox';
-    const lightboxClose = document.createElement('span');
-    lightboxClose.className = 'lightbox-close';
-    lightboxClose.innerHTML = '&times;';
+const galleryImages = document.querySelectorAll(".gallery img");
+const lightbox = document.createElement("div");
+lightbox.className = "lightbox";
+const lightboxClose = document.createElement("span");
+lightboxClose.className = "lightbox-close";
+lightboxClose.innerHTML = "&times;";
 
-    galleryImages.forEach(image => {
-      image.addEventListener('click', () => {
-        const clone = image.cloneNode();
-        lightbox.innerHTML = '';
-        lightbox.appendChild(clone);
-        lightbox.appendChild(lightboxClose);
-        document.body.appendChild(lightbox);
-        lightbox.style.display = 'flex';
-      });
-    });
+galleryImages.forEach((image) => {
+  image.addEventListener("click", () => {
+    const clone = image.cloneNode();
+    lightbox.innerHTML = "";
+    lightbox.appendChild(clone);
+    lightbox.appendChild(lightboxClose);
+    document.body.appendChild(lightbox);
+    lightbox.style.display = "flex";
+  });
+});
 
-    lightbox.addEventListener('click', event => {
-      if (event.target === lightbox || event.target === lightboxClose) {
-        lightbox.style.display = 'none';
-      }
-    });
+lightbox.addEventListener("click", (event) => {
+  if (event.target === lightbox || event.target === lightboxClose) {
+    lightbox.style.display = "none";
+  }
+});
